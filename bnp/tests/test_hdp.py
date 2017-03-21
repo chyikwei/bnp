0e0686a268fc23d4058e20829448c63f51b98850
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2017-03-21 16:54:09 -0400

    update tests

--- bnp/tests/test_hdp.py
+++ bnp/tests/test_hdp.py
@@ -1,72 +1,39 @@
 import numpy as np
-from scipy.special import psi
+
 from sklearn.externals.six.moves import xrange
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.testing import (assert_almost_equal, assert_raises_regexp,
-                                   assert_equal)
+                                   assert_equal, assert_true)
+from sklearn.utils import shuffle
 
 from bnp.online_hdp import HierarchicalDirichletProcess
-from bnp.utils.sample_generator import make_uniform_doc_word_matrix
+from bnp.utils.sample_generator import make_doc_word_matrix, make_uniform_doc_word_matrix
 
 
-def _build_sparse_mtx(n_topics=10):
-    X = make_uniform_doc_word_matrix(
-        n_topics=n_topics, words_per_topic=3, docs_per_topic=3)
-    return (n_topics, X)
+def _hdp_topic_check(hdp_model, n_topics, words_per_topic,
+                     topics_threshold=0.1):
+    """Check large topic inside HDP is grouped correctly"""
 
-
-def test_hdp_fit():
-    """Test HDP fit"""
-
-    _, X = _build_sparse_mtx()
-    params = {
-        'n_topic_truncate': 20,
-        'n_doc_truncate': 5,
-        'learning_method': 'batch',
-        'max_iter': 10,
-    }
-    hdp = HierarchicalDirichletProcess(**params)
-    hdp.fit(X)
-
-
-def test_hdp_partial_fit():
-    """Test HDP partial_fit"""
-
-    _, X = _build_sparse_mtx()
-    params = {
-        'n_topic_truncate': 20,
-        'n_doc_truncate': 5,
-        'learning_method': 'online',
-    }
-    hdp = HierarchicalDirichletProcess(**params)
-    for _ in xrange(10):
-        hdp.partial_fit(X)
-
-
-def test_hdp_transform():
-    """Test HDP transform"""
-
-    _, X = _build_sparse_mtx()
-    params = {
-        'n_topic_truncate': 20,
-        'n_doc_truncate': 5,
-        'learning_method': 'batch',
-        'max_iter': 10,
-    }
-    hdp = HierarchicalDirichletProcess(**params)
-
-    assert_raises_regexp(NotFittedError, r"^no 'lambda_' attribute",
-                         hdp.transform, X)
-    hdp.fit(X)
-    transformed = hdp.transform(X)
-    assert_equal(transformed.shape[0], X.shape[0])
-    assert_equal(transformed.shape[1], 20)
+    topic_distr = hdp_model.topic_distribution()
+    topic_covers = np.zeros(n_topics)
+    for idx in xrange(hdp_model.lambda_.shape[0]):
+        if topic_distr[idx] < topics_threshold:
+            continue
+        topic = hdp_model.lambda_[idx, :]
+        top_word_idxs = topic.argsort()[:-words_per_topic - 1:-1]
+        max_idx = np.max(top_word_idxs)
+        min_idx = np.min(top_word_idxs)
+        assert_equal(max_idx - min_idx, words_per_topic - 1)
+        topic_covers[int(min_idx / words_per_topic)] = 1
+    assert_true((topic_covers > 0.).all())
 
 
 def test_hdp_fit_transform():
     """Test HDP fit_transform"""
 
-    _, X = _build_sparse_mtx()
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+
     params = {
         'n_topic_truncate': 20,
         'n_doc_truncate': 5,
@@ -83,9 +50,56 @@ def test_hdp_fit_transform():
     assert_almost_equal(transformed_1, transformed_2)
 
 
+def test_hdp_dense_input():
+    """Dense and sparse input should be the same"""
+
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+    dense_X = X.todense()
+
+    params = {
+        'n_topic_truncate': 20,
+        'n_doc_truncate': 5,
+        'learning_method': 'batch',
+        'max_iter': 10,
+        'random_state': 1,
+    }
+    hdp1 = HierarchicalDirichletProcess(**params)
+    transformed_1 = hdp1.fit_transform(dense_X)
+
+    hdp2 = HierarchicalDirichletProcess(**params)
+    transformed_2 = hdp2.fit_transform(X)
+    assert_almost_equal(transformed_1, transformed_2)
+
+
+def test_hdp_transform():
+    """Test HDP transform"""
+
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+
+    params = {
+        'n_topic_truncate': 20,
+        'n_doc_truncate': 5,
+        'learning_method': 'batch',
+        'max_iter': 10,
+    }
+    hdp = HierarchicalDirichletProcess(**params)
+
+    assert_raises_regexp(NotFittedError, r"^no 'lambda_' attribute",
+                         hdp.transform, X)
+    hdp.fit(X)
+    transformed = hdp.transform(X)
+    assert_equal(transformed.shape[0], X.shape[0])
+    assert_equal(transformed.shape[1], 20)
+
+
 def test_hdp_topic_distribution():
     """Test HDP topic_distribution"""
-    _, X = _build_sparse_mtx()
+
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+
     params = {
         'n_topic_truncate': 20,
         'n_doc_truncate': 5,
@@ -108,7 +122,10 @@ def test_partial_fit_after_fit():
 
     partial_fit should reset global parameters
     """
-    _, X = _build_sparse_mtx()
+
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+
     params = {
         'n_topic_truncate': 20,
         'n_doc_truncate': 5,
@@ -130,7 +147,9 @@ def test_enable_likelihood():
     The result should be the same no matter it
     is True or False.
     """
-    _, X = _build_sparse_mtx()
+    X = make_uniform_doc_word_matrix(
+        n_topics=10, words_per_topic=3, docs_per_topic=3)
+
     params = {
         'n_topic_truncate': 20,
         'n_doc_truncate': 5,
@@ -146,3 +165,55 @@ def test_enable_likelihood():
     hdp2 = HierarchicalDirichletProcess(**params)
     ret2 = hdp2.fit_transform(X)
     assert_almost_equal(ret1, ret2)
+
+
+def test_hdp_fit_topics_with_fake_data():
+    """Test HDP fit with fake data
+
+    Top words in large topics should be grouped correctly
+    (small topic can be ignored.)
+    """
+    n_topics = 3
+    n_topic_truncate = 10
+    topics_threshold = 0.1
+    words_per_topic = 10
+    tf = make_doc_word_matrix(n_topics=n_topics,
+                              words_per_topic=words_per_topic,
+                              docs_per_topic=100,
+                              words_per_doc=50,
+                              shuffle=True,
+                              random_state=0)
+
+    hdp = HierarchicalDirichletProcess(n_topic_truncate=n_topic_truncate,
+                                       n_doc_truncate=3,
+                                       max_iter=5,
+                                       random_state=0)
+
+    hdp.fit(tf)
+    _hdp_topic_check(hdp, n_topics, words_per_topic, topics_threshold)
+
+
+def test_hdp_partial_fit_with_fake_data():
+    """Test HDP partial_fit with fake data
+    
+    Same as `test_hdp_fit_topics_with_fake_data` but
+    use `partial_fit` to replace `fit`
+    """
+
+    n_topics = 3
+    n_topic_truncate = 10
+    topics_threshold = 0.1
+    words_per_topic = 10
+    tf = make_doc_word_matrix(n_topics=n_topics,
+                              words_per_topic=words_per_topic,
+                              docs_per_topic=100,
+                              words_per_doc=50,
+                              shuffle=True,
+                              random_state=0)
+
+    hdp = HierarchicalDirichletProcess(n_topic_truncate=n_topic_truncate,
+                                       n_doc_truncate=3,
+                                       random_state=0)
+    for _ in xrange(5):
+        hdp.partial_fit(tf)
+    _hdp_topic_check(hdp, n_topics, words_per_topic, topics_threshold)
