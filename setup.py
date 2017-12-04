23472105b9e8d199e14f8f30cdb9751d6e28451d
Chyi-Kwei Yau <chyikwei.yau@gmail.com>
2017-12-04 00:16:37 -0500

    remove repeated install_requires arg

--- setup.py
+++ setup.py
@@ -45,12 +45,6 @@ setup(
     name='bnp',
     version=bnp.__version__,
     url='https://github.com/chyikwei/bnp/',
-    install_requires=[
-        'numpy>=1.11.2',
-        'scipy>=0.18.1',
-        'scikit-learn>=0.18.1',
-        'Cython>=0.20.2'
-    ],
     ext_modules=cythonize(extensions),
     cmdclass={'build_ext': build_ext},
     test_suite='nose.collector',
